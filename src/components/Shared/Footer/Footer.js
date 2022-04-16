caae89c91414fff42987259f4892b1ff108afc12
pallobbiswas <pallobbiswas326@gmail.com>
2022-04-16 22:50:14 +0600

    footer done

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-    return (
+  return (
+    <div className="footrt-bg-color mt-5">
+      <div className="container text-center py-3">
         <div>
-            <h1>footer</h1>
+          <a href="#facebook">
+            <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon>
+          </a>
+          <a href="#linkdin">
+            <FontAwesomeIcon className="mx-3" icon={faCoffee}></FontAwesomeIcon>
+          </a>
+          <a href="#twiter">
+            <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon>
+          </a>
         </div>
-    );
+        <p>
+          <small>Everythig is copyright &copy;2022</small>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
